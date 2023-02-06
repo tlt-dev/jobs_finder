219a2fb805bd26bb1e3ed1ec1f7597f375ed0bd0
Lucas Raynaud <lucas.raynaud24@gmail.com>
2023-02-06 11:29:11 +0100

    Fix OffreObjet typo

--- int/mod_offre/controleur/OffreObjet.php
+++ int/mod_offre/controleur/OffreObjet.php
@@ -36,19 +36,19 @@ class OffreEmploiObjet
         }
     }
 
-    public function get_Off_id(){
+    public function getOff_id(){
         return $this->off_id;
     }
 
-    public function set_Off_id($off_id){
+    public function setOff_id($off_id){
         $this->$off_id = $off_id;
     }
 
-    public function get_Off_intitule(){
+    public function getOff_intitule(){
         return $this->off_intitule;
     }
 
-    public function set_Off_intitule($off_intitule){
+    public function setOff_intitule($off_intitule){
         if (strlen($off_intitule) > 100){
             self::setMessageErreur("Intitulé trop long !");
             $this->setAutorisationBD(false);
@@ -57,35 +57,35 @@ class OffreEmploiObjet
         }
     }
 
-    public function get_Off_ville(){
+    public function getOff_ville(){
         return $this->off_ville;
     }
 
-    public function set_Off_ville($off_ville){
+    public function setOff_ville($off_ville){
         $this->$off_ville = $off_ville;
     }
 
-    public function get_Off_cp_ville(){
+    public function getOff_cp_ville(){
         return $this->off_cp_ville;
     }
 
-    public function set_Off_cp_ville($off_cp_ville){
+    public function setOff_cp_ville($off_cp_ville){
         $this->$off_cp_ville = $off_cp_ville;
     }
 
-    public function get_Off_date_prise_poste(){
+    public function getOff_date_prise_poste(){
         return $this->off_date_prise_poste;
     }
 
-    public function set_Off_date_prise_poste($off_date_prise_poste){
+    public function setOff_date_prise_poste($off_date_prise_poste){
         $this->$off_date_prise_poste = $off_date_prise_poste;
     }
 
-    public function get_Off_salaire(){
+    public function getOff_salaire(){
         return $this->off_salaire;
     }
 
-    public function set_Off_salaire($off_salaire){
+    public function setOff_salaire($off_salaire){
         if ($off_salaire < 0){
             self::setMessageErreur("Salaire négatif !");
             $this->setAutorisationBD(false);
@@ -94,35 +94,35 @@ class OffreEmploiObjet
         }
     }
 
-    public function get_Off_duree(){
+    public function getOff_duree(){
         return $this->off_duree;
     }
 
-    public function set_Off_duree($off_duree){
+    public function setOff_duree($off_duree){
         $this->$off_duree = $off_duree;
     }
 
-    public function get_Off_secteur_activite(){
+    public function getOff_secteur_activite(){
         return $this->off_secteur_activite;
     }
 
-    public function set_Off_secteur_activite($off_secteur_activite){
+    public function setOff_secteur_activite($off_secteur_activite){
         $this->$off_secteur_activite = $off_secteur_activite;
     }
 
-    public function get_Off_entreprise(){
+    public function getOff_entreprise(){
         return $this->off_entreprise;
     }
 
-    public function set_Off_entreprise($off_entreprise){
+    public function setOff_entreprise($off_entreprise){
         $this->$off_entreprise = $off_entreprise;
     }
 
-    public function get_Off_descriptif(){
+    public function getOff_descriptif(){
         return $this->off_descriptif;
     }
 
-    public function set_Off_descriptif($off_descriptif){
+    public function setOff_descriptif($off_descriptif){
         if (strlen($off_descriptif) > 1000){
             self::setMessageErreur("Descriptif trop long !");
             $this->setAutorisationBD(false);
